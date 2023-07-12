527f810a662c474c0f695309d5ab6bca7e96b9ab
LautharoFerryra <138071378+LautharoFerryra@users.noreply.github.com>
2023-07-12 10:01:02 -0300

    DatosJava

---
+++ MySqlJava/Java/Datos.java
@@ -0,0 +1,12 @@
+package CodingDojo.MySqlJava.Java;
+
+public class Datos {
+    public static void main(String[] args) {
+        System.out.println("Estos son mis datos: ");
+        System.out.println("Mi nombre es Lautharo ");
+        System.out.println("Tengo 20 a\u00F1os ");
+        System.out.println("Soy de Montevideo, Uruguay");
+    }
+}
+
+
